20091b3629e3275ea516d1d19daf03f54f4de7d3
EBroere <e.broere91@gmail.com>
2024-02-25 18:55:46 +0100

    fixed the api

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function search(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
   let city = searchInputElement.value;
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "bd30oafc2f7e9f6a35bab68ba504ft6c";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(function (response) {
@@ -111,7 +111,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function searchCity(city) {
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "bd30oafc2f7e9f6a35bab68ba504ft6c";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(function (response) {
